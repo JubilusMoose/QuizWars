0c981ae16face817441b4eecd161a82a691ec9e4
Jonathan <jonathan.eleiott@gmail.com>
2017-05-13 00:24:05 -0700

    Set up static pages and server routes

--- tugOfWarQuiz/server/server.js
+++ tugOfWarQuiz/server/server.js
@@ -1,11 +1,29 @@
-var express = require('express');
-var path = require('path');
-var app = express();
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
 
-app.get('/', function(req, res) {
-  res.sendFile(path.resolve('../public/index.html'));
+app.set('views', __dirname + '/../public');
+app.set('view engine', 'html');
+
+app.use(express.static(path.join(__dirname, '/../public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const headers = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10, // Seconds.
+  'Content-Type': 'application/json'
+};
+
+app.get('/', (req, res) => {
+  console.log('req');
+  res.writeHead(200, headers);
+  res.render('../public/app/home/home.html');
 });
 
-app.listen(3000, function() {
+app.listen(3000, () => {
   console.log('app listening on port 3000');
 });
