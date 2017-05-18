0f7850e98bd347cc8c92318e46e20c9fde5cfe89
Jonathan <jonathan.eleiott@gmail.com>
2017-05-18 03:14:46 -0700

    Server call from home console logs and then sends user to newGame

--- tugOfWarQuiz/public/app/app.js
+++ tugOfWarQuiz/public/app/app.js
@@ -1,11 +1,2 @@
 
 angular.module('tugOfWarApp', ['ngRoute'])
-.config(function($routeProvider) {
-  $routeProvider
-    .when('/newGame', {
-      templateUrl: './app/newGame/newGame.html'
-    })
-    .otherwise({
-      templateUrl: './app/home/home.html'
-    })
-})
