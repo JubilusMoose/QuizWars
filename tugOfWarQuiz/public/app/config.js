26f771a175f003dc76ce3632a2f955d12613293f
Jonathan <jonathan.eleiott@gmail.com>
2017-05-19 23:54:45 -0700

    Set up login html and controller

--- tugOfWarQuiz/public/app/config.js
+++ tugOfWarQuiz/public/app/config.js
@@ -2,7 +2,8 @@ angular.module('tugOfWarApp')
 .config(function($routeProvider) {
   $routeProvider
     .when('/newGame', {
-      templateUrl: './app/newGame/newGame.html'
+      templateUrl: './app/newGame/newGame.html',
+      controller: './app/newGame/newGameController.js'
     })
     .otherwise({
       templateUrl: './app/home/home.html',
