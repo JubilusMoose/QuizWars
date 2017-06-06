02b481aa5423eb42d53f8f42d5008132b301a184
Jonathan <jonathan.eleiott@gmail.com>
2017-06-05 20:09:41 -0700

    Email requires @ symbol

--- tugOfWarQuiz/public/app/loginAndSignup/loginController.js
+++ tugOfWarQuiz/public/app/loginAndSignup/loginController.js
@@ -2,15 +2,20 @@ angular.module('tugOfWarApp').controller('loginController', function($rootScope,
   console.log('inside login controller');
   $scope.access = 'student';
   $scope.loginAttempt = function(email, password) {
-    var access = $scope.access;
-    axios.post('/login', {
-      email,
-      password,
-      access
-    })
-    console.log(email, password, access)
-    $rootScope.loggedIn = true;
-    $location.path('/home')
+    if(!email.contains('@')) {
+      console.log('not an email address');
+      $location.path('/login');
+    } else {
+      var access = $scope.access;
+      axios.post('/login', {
+        email,
+        password,
+        access
+      })
+      console.log(email, password, access)
+      $rootScope.loggedIn = true;
+      $location.path('/home');
+    }
   }
 
   $scope.signupAttempt = function(email, password) {
