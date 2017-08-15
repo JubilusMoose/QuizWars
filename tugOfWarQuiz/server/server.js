8d5b6483683eba01cc6e8397b35b9defb3ffeea3
Jonathan <jonathan.eleiott@gmail.com>
2017-08-14 23:53:53 -0700

    Send questions and answers to server when creating a new game

--- tugOfWarQuiz/server/server.js
+++ tugOfWarQuiz/server/server.js
@@ -42,25 +42,30 @@ app.get('/allJoinedGames', (req, res) => {
 })
 
 // Calls from FE
-app.post('/login', (req, res) => {
-  console.log('check user in database', req.body);
-  helpers.login(req, res);
-})
-
 app.post('/signup', (req, res) => {
   console.log('save user to database', req.body);
   helpers.signup(req, res);
-})
+});
+
+app.post('/login', (req, res) => {
+  console.log('check user in database', req.body);
+  helpers.login(req, res);
+});
 
 app.post('/changeName', (req, res) => {
   console.log('change name in db', req.body);
   helpers.changeName(req, res);
-})
+});
 
 app.post('/createGame', (req, res) => {
   console.log('create game', req.body);
   helpers.createGame(req, res);
-})
+});
+
+app.post('/addQuestionsToGame', (req, res) => {
+  console.log('add questions to game', req.body);
+  sendResponse(res, 200, headers, 'call success');
+});
 
 app.post('/joinRoom', (req, res) => {
   console.log('post request to joinRoom successfully ran');
@@ -69,7 +74,7 @@ app.post('/joinRoom', (req, res) => {
 
 app.get('*', (req, res) => {
   res.render('index.html');
-})
+});
 
 app.listen(3000, () => {
   console.log('app listening on port 3000');
